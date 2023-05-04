a510462e065ae899a472a6a450ed3a28d5a30d2c
noname21-stack <vanhuybuivips@gmail.com>
2023-05-04 22:36:54 +0700

    change AI

--- parking/ModelAI/model_AI.py
+++ parking/ModelAI/model_AI.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from .lib_detection import load_model_1, detect_lp, im2single
 from .lib_findcontours import segment_characters
-
+import sys
 def mode_AI(img):
     wpod_net_path = "./File/wpod-net_update1.json"
     wpod_net = load_model_1(wpod_net_path)
@@ -19,8 +19,13 @@ def mode_AI(img):
     ratio = float(max(Ivehicle.shape[:2])) / min(Ivehicle.shape[:2])
     side = int(ratio * Dmin)
     bound_dim = min(side, Dmax)
-
-    _, LpImg, lp_type = detect_lp(wpod_net, im2single(Ivehicle), bound_dim, lp_threshold=0.5)
+    LpImg = None
+    lp_type = None
+    try:
+        _, LpImg, lp_type = detect_lp(wpod_net, im2single(Ivehicle), bound_dim, lp_threshold=0.5)
+    except Exception:
+        print("Không nhận diện được biển số")
+        sys.exit(1)
 
     # Cau hinh tham so cho model SVM
     digit_w = 30  # Kich thuoc ki tu
@@ -31,10 +36,14 @@ def mode_AI(img):
 
         # Chuyen doi anh bien so
         LpImg[0] = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
-        roi = LpImg[0]
+        img = LpImg[0]
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), cv2.BORDER_DEFAULT)
+        roi = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                    cv2.THRESH_BINARY, 199, 5)
         if (lp_type == 2):
             char = []
-            height, width, _ = roi.shape
+            height, width, _ = img.shape
             midpoint = height // 2
 
             # Chia đôi ảnh theo chiều ngang
@@ -42,15 +51,20 @@ def mode_AI(img):
             bottom_half = roi[midpoint - 10:, :]
 
             # Tim counter cua tung anh
-            char_top = segment_characters(top_half, 200, 10)
-            char_bottom = segment_characters(bottom_half, 200, 10)
+            char_top, type_check = segment_characters(top_half, 200, 11, 2)
+            char_bottom, type_check = segment_characters(bottom_half, 200, 10, 2)
+            if char_bottom.shape[0] > 5:
+                char_bottom = char_bottom[0:char_bottom.shape[0] - 1]
 
             for i in range(0, char_top.shape[0]):
                 char.append(char_top[i])
             for i in range(0, char_bottom.shape[0]):
                 char.append(char_bottom[i])
+            char = np.array(char)
         else:
-            char = segment_characters(roi, 333, 12)
+            char, type_check = segment_characters(roi, 333, 16, 1)
+        if char.shape[0] > 8 and type_check == 1:
+            char = char[0:char.shape[0] - 1]
 
         plate_info = ""
         for curr_num in char:
@@ -58,7 +72,6 @@ def mode_AI(img):
             curr_num = np.array(curr_num, dtype=np.float32)
             curr_num = curr_num.reshape(-1, digit_w * digit_h)
             result = model_svm.predict(curr_num)[1]
-            # print(result)
             result = int(result[0, 0])
 
             if result <= 9:  # Neu la so thi hien thi luon
--- parking/manager/__init__.py
+++ parking/manager/__init__.py
@@ -1,3 +1,4 @@
-import pymysql
 
+import pymysql
+pymysql.version_info = (1, 4, 3, "final", 0)
 pymysql.install_as_MySQLdb()
